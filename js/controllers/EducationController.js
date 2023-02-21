da32e63b1a7a686b7deb0d058c551cc9f6db828c
Siddhartha Nepal <siddharthanepal05@gmail.com>
2023-02-21 10:36:16 -0500

    updated

--- js/controllers/EducationController.js
+++ js/controllers/EducationController.js
@@ -3,8 +3,7 @@ app.controller('EducationController', ['$scope', function($scope) {
 
   let mySchool = {
     school:"University of Central Missouri",
-    degree: ["Master of Science in Big Data Analytics",
-             "Master of Science in Computer Information System"]
+    degree: ["Master of Science in Computer Information System"]
   }
   $scope.education = mySchool;
 }]);
